c5818521a35e0d64ca8bb4ae424360874ca0d5af
VAIBHAV RSP <112012170+vaibhavRSP@users.noreply.github.com>
2025-09-24 17:38:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+def twosum(nums, target):
+    n =len(nums)
+    for i in range(n):
+        for j in range(i+1,n):
+            if nums[i] + nums[j] == target:
+                return[i,j]
+                
+
+
+        
